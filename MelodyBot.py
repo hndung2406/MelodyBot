b18ae679daeaee5c458cb115ef6b596fd9f8eb85
Dung Hoang <hndung2406@gmail.com>
2019-01-25 15:38:55 +0700

    Add Basic Music

--- MelodyBot.py
+++ MelodyBot.py
@@ -13,6 +13,26 @@ client = commands.Bot(command_prefix='!')
 client.remove_command('help')
 status = ['Dota 2', 'Artifact', 'Hearthstone']
 
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
 
 def report_guild(guild):
     online = 0
@@ -99,6 +119,8 @@ async def clear(ctx):
 @client.command()
 async def join(ctx):
     channel = ctx.message.author.voice.channel
+    if ctx.voice_client is not None:
+        return await ctx.voice_client.move_to(channel)
     await channel.connect()
 
 
@@ -111,10 +133,20 @@ async def leave(ctx):
 
 @client.command()
 async def play(ctx, url):
-    guild = ctx.message.guild
-    voice_client = guild.voice_client
-    player = await voice_client.create_ytdl_player(url)
-    player.start()
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=client.loop)
+        ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+    await ctx.send('Now playing: {}'.format(player.title))
+
+
+@client.command()
+async def stream(ctx, url):
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=client.loop, stream=True)
+        ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+    await ctx.send('Now playing: {}'.format(player.title))
 
 
 # Change the status of the bot every 5 mins
@@ -125,6 +157,28 @@ async def change_status():
         current_status = next(msgs)
         await client.change_presence(status=discord.Status.online, activity=discord.Game(name=current_status))
         await asyncio.sleep(300)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 client.loop.create_task(change_status())
